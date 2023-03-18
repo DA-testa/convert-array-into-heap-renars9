535ad52f5577a9ec102917a894346df7c4c0201e
Renārs Saukāns 221RDB012 <125391627+renars9@users.noreply.github.com>
2023-03-18 14:47:05 +0000

    commit build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,11 +1,23 @@
 # python3
-
+import numpy
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    smallest = i  
+    left = 2 * i + 1  
+    right = 2 * i + 2 
 
+    if left < n and arr[left] < arr[smallest]:
+        smallest = left
+
+    if right < n and arr[right] < arr[smallest]:
+        smallest = right
+
+    if smallest != i:
+        arr[i], arr[smallest] = arr[smallest], arr[i]
+        swaps.append((i, smallest))
+        sub_swaps = heapify(arr, n, smallest)
+        swaps.extend(sub_swaps)
 
     return swaps
 
@@ -20,6 +32,7 @@ def main():
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
+    arr = numpy.fromstring(data, dtype=int, sep=' ')
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
